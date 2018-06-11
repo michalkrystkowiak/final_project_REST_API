ccd888aa8ae78406c284abcc6d65f37c34f29242
Michal Krystkowiak <michalkrystkowiakmail@gmail.com>
2018-06-11 23:08:30 +0200

    v.1.1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log("Fetch")
     fetch("https://allegro.pl/auth/oauth/token?grant_type=authorization_code&code=" + firstCodeAuthorization + "&redirect_uri=https://familiare-store.com/ProjektRestApiAllegro/index.html", {
     headers: {
-      Authorization: "13a9a1cb918c45d3a75c1b851e1c1000:QDb8nioxJfVa2Ok6dWcF8Fq9Q9j2VSc4Jxp4iubR2ugU1wsC4yRCGqz2P8uag5CX"
+      Authorization: "Basic " + btoa("13a9a1cb918c45d3a75c1b851e1c1000:QDb8nioxJfVa2Ok6dWcF8Fq9Q9j2VSc4Jxp4iubR2ugU1wsC4yRCGqz2P8uag5CX")
     },
       method: "POST"
     })  
